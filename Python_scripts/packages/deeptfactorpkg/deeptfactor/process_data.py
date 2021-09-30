c0675705b916d52425b741ce76ea4cd9ded1d1ac
JustinTzeJi <justintzeji@gmail.com>
2021-09-30 22:32:36 +0800

    update DeepTF to replace U to X

--- Python_scripts/packages/deeptfactorpkg/deeptfactor/process_data.py
+++ Python_scripts/packages/deeptfactorpkg/deeptfactor/process_data.py
@@ -8,15 +8,15 @@ def read_fasta_data(fasta_file, len_criteria=1000):
     result = []
     seq_ids = []
     seq_slices = []
-    fp = open(fasta_file, 'r')
-    for seq_record in SeqIO.parse(fp, 'fasta'):
-        seq = seq_record.seq
-        seq_id = seq_record.id
-        seq_slice = seq_record.description
-        if len(seq) <= len_criteria:
-            seq += '_' * (len_criteria-len(seq))
-            result.append(str(seq))
-            seq_ids.append(seq_id)
-            seq_slices.append(seq_slice)
-    fp.close()
+    with open(fasta_file, 'r') as fp:
+        for seq_record in SeqIO.parse(fp, 'fasta'):
+            seq = seq_record.seq
+            seq_id = seq_record.id
+            seq_slice = seq_record.description
+            if len(seq) <= len_criteria:
+                seq = seq.replace("U", "X")
+                seq += '_' * (len_criteria-len(seq))
+                result.append(str(seq))
+                seq_ids.append(seq_id)
+                seq_slices.append(seq_slice)
     return result, seq_ids, seq_slices
